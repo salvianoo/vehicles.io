76630c7a579975f5abe01749d72ef52984cfba8d
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-11-12 19:12:10 -0200

    Fixed migration

--- db/migrations/002_remove_password_column.rb
+++ db/migrations/002_remove_password_column.rb
@@ -1,7 +1,7 @@
 Sequel.migration do
   change do
-    add_column :users, :password_salt, null: false, String
-    add_column :users, :password_hash, null: false, String
+    add_column :users, :password_salt, :null => false, String
+    add_column :users, :password_hash, :null => false, String
 
     drop_column :users, :password
   end
