15f0a9413dfd58d6ac7a58f1d6c400bf2e866c14
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-11-17 19:05:14 -0200

    Setting a database url for running tests with semaphore ci

--- config/database.rb
+++ config/database.rb
@@ -5,6 +5,9 @@ require 'dotenv'
 Dotenv.load
 
 environment = ENV.fetch('RACK_ENV') { 'development' }
-connection_string = ENV["DATABASE_URL"] || ENV["DATABASE_URL_#{environment.upcase}"]
+
+connection_string = ENV["DATABASE_URL"] ||
+                    ENV["DATABASE_URL_#{environment.upcase}"] ||
+                    ENV["DATABASE_URL_CI_TEST"]
 
 DB = Sequel.connect(connection_string)
