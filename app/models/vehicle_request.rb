68126c55caf6aab533985e3ad64750a80973a7d3
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-11-20 15:33:38 -0200

    Add documentation FTW

--- app/models/vehicle_request.rb
+++ app/models/vehicle_request.rb
@@ -12,6 +12,23 @@ class VehicleRequest < Sequel::Model
     super(values)
   end
 
+  # To create new passengers instances related to the VehicleRequest object
+  # I need to do some like this:
+  #
+  #  request = VehicleRequest.new(params)
+  #
+  #  request.passengers_attributes = [{name: 'Joao'}, {name: 'Maria'}]
+  #
+  # With this method it could be better, after define the request object,
+  # just call it.
+  #
+  #  request.create_nested_passengers
+  #
+  # The map function will return an array in the format above.
+  # Now i can retrieve the passengers nested to the current object
+  #
+  #  request.passengers
+  #
   def create_nested_passengers
     self.passengers_attributes = @passengers.map {|p_name| {name: p_name}}
   end
