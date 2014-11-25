0c41d01ab13a8f9842fbaeec0556b5810a68e66b
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-11-25 18:01:52 -0200

    Added input masks with jquery-maskedinput'

--- public/js/app.js
+++ public/js/app.js
@@ -4,14 +4,7 @@ var hideAllRadios = function() {
   $('.info-conduzir').hide();
 };
 
-var ready = function() {
-  hideAllRadios();
-
-  $('.remove_fields').click(function(event) {
-    $(this).prev('input[type=hidden]').val('1');
-    $(this).closest('.row').hide();
-    event.preventDefault();
-  });
+var hideAndShowOptions = function() {
 
   $('.buscar-option').click(function() {
     hideAllRadios();
@@ -27,6 +20,41 @@ var ready = function() {
     hideAllRadios();
     $('.info-conduzir').show();
   });
+
+};
+
+var dateTimeMask = function() {
+  $("#date").mask("99/99/9999");
+  $("#hour").mask("99:99");
+};
+
+var enableDadosVoo = function() {
+  $("#sim_buscar").click(function () {
+    $("#cia_area").removeAttr("disabled");
+    $("#num_voo").removeAttr("disabled");
+  });
+};
+
+var disableDadosVoo = function() {
+  $("#nao_buscar").click(function () {
+    $("#cia_area").attr('disabled', true);
+    $("#num_voo").attr('disabled', true);
+  });
+}
+
+var ready = function() {
+  hideAllRadios();
+
+  $('.remove_fields').click(function( event ) {
+    $(this).prev('input[type=hidden]').val('1');
+    $(this).closest('.row').hide();
+    event.preventDefault();
+  });
+
+  hideAndShowOptions();
+  dateTimeMask();
+  enableDadosVoo();
+  disableDadosVoo();
 };
 
 $(document).ready(ready);
