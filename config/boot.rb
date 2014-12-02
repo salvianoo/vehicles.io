c5f35ea7d224eae89842a278eb44b0a07d5ba7d2
Salviano Ludgerio <salviano.lfg@gmail.com>
2014-12-02 10:15:12 -0200

    Quick fixes

--- config/boot.rb
+++ config/boot.rb
@@ -4,7 +4,6 @@ require_relative 'bugsnag'
 
 # Set up the controllers and helpers
 Dir[APP_ROOT.join('app', 'controllers', '*.rb')].each { |file| require file }
-Dir[APP_ROOT.join('app', 'helpers', '*.rb')].each { |file| require file }
-Dir[APP_ROOT.join('app', 'services', '*.rb')].each { |file| require file }
-
-Dir[APP_ROOT.join('app', 'models', '*.rb')].each { |file| require file }
+Dir[APP_ROOT.join('app', 'helpers',     '*.rb')].each { |file| require file }
+Dir[APP_ROOT.join('app', 'services',    '*.rb')].each { |file| require file }
+Dir[APP_ROOT.join('app', 'models',      '*.rb')].each { |file| require file }
